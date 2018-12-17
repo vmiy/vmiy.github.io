8370b482cc5f70f3c050409a9bc32bdaa9502e49
v-miy <v-miy@microsoft.com>
2018-12-17 15:12:40 +0800

    Updates

---
+++ precache-manifest.e297a28405715be0d45c7201a2dfcf90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "3b3d9902f5bd88d56d0a",
+    "url": "/react-tutorial/static/js/main.3b3d9902.chunk.js"
+  },
+  {
+    "revision": "2818dbc80ba3c939eefa",
+    "url": "/react-tutorial/static/js/1.2818dbc8.chunk.js"
+  },
+  {
+    "revision": "3b3d9902f5bd88d56d0a",
+    "url": "/react-tutorial/static/css/main.70cdf8b7.chunk.css"
+  },
+  {
+    "revision": "e501056848c5336da281b7a0ef96c419",
+    "url": "/react-tutorial/index.html"
+  }
+];
